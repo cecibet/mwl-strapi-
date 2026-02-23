a8682b8bfd9546a1289dcd5f8a6c6d9182012739
Cecilia Bettiol <cecilia.bettiol@thermofisher.com>
2026-02-23 18:18:46 -0300

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -37,11 +37,11 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
           ca: env('DATABASE_SSL_CA', undefined),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+              rejectUnauthorized: false,
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: { min: 2, max: 10 },
     },
     sqlite: {
       connection: {
